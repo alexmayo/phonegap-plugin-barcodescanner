a748069853f1e2f0b2af9ec34753cf14f938662b
Alexander Mayo <al.mayo@icloud.com>
2019-08-30 12:01:53 +0100

    Update CDVBarcodeScanner.mm

--- src/ios/CDVBarcodeScanner.mm
+++ src/ios/CDVBarcodeScanner.mm
@@ -993,9 +993,8 @@ parentViewController:(UIViewController*)parentViewController
     //TOP LEFT OUTER
     if (self.processor.is2D) {
         UIColor* color = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:RETICLE_ALPHA];
-        CGContextSetStrokeColorWithColor(context, color.CGColor);
-        CGContextSetLineWidth(context, RETICLE_WIDTH);
-        CGContextStrokeRect(context,
+        CGContextSetFillColorWithColor(context, color.CGColor);
+        CGContextFillRect(context,
                             CGRectMake(
                                        RETICLE_OFFSET,
                                        RETICLE_OFFSET,
@@ -1006,13 +1005,13 @@ parentViewController:(UIViewController*)parentViewController
     }
     //TOP LEFT INNER
     if (self.processor.is2D) {
-        UIColor* color = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:RETICLE_ALPHA];
+        UIColor* color = [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:RETICLE_ALPHA];
         CGContextSetStrokeColorWithColor(context, color.CGColor);
         CGContextSetLineWidth(context, RETICLE_WIDTH);
         CGContextStrokeRect(context,
                             CGRectMake(
-                                       RETICLE_OFFSET+15.0f,
-                                       RETICLE_OFFSET+15.0f,
+                                       RETICLE_OFFSET+10.0f,
+                                       RETICLE_OFFSET+10.0f,
                                        3.0f,
                                        3.0f
                                        )
