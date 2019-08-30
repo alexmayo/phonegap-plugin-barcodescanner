04b58e2832e7db91b760abb2ed1767d6d8587326
Alexander Mayo <al.mayo@icloud.com>
2019-08-30 11:50:50 +0100

    Update CDVBarcodeScanner.mm

--- src/ios/CDVBarcodeScanner.mm
+++ src/ios/CDVBarcodeScanner.mm
@@ -954,7 +954,7 @@ parentViewController:(UIViewController*)parentViewController
 
 #define RETICLE_SIZE    500.0f
 #define RETICLE_WIDTH     5.0f
-#define RETICLE_OFFSET   90.0f
+#define RETICLE_OFFSET   150.0f
 #define RETICLE_ALPHA     1.0f
 
 //-------------------------------------------------------------------------
@@ -990,9 +990,8 @@ parentViewController:(UIViewController*)parentViewController
     //                        );
     //}
     
-    //Top Left QR Icon
+    //TOP LEFT OUTER
     if (self.processor.is2D) {
-        //Border
         UIColor* color = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:RETICLE_ALPHA];
         CGContextSetStrokeColorWithColor(context, color.CGColor);
         CGContextSetLineWidth(context, RETICLE_WIDTH);
@@ -1000,21 +999,20 @@ parentViewController:(UIViewController*)parentViewController
                             CGRectMake(
                                        RETICLE_OFFSET,
                                        RETICLE_OFFSET,
-                                       20.0f,
-                                       20.0f
+                                       30.0f,
+                                       30.0f
                                        )
                             );
     }
-    
+    //TOP LEFT INNER
     if (self.processor.is2D) {
-        //Border
         UIColor* color = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:RETICLE_ALPHA];
         CGContextSetStrokeColorWithColor(context, color.CGColor);
         CGContextSetLineWidth(context, RETICLE_WIDTH);
         CGContextStrokeRect(context,
                             CGRectMake(
-                                       RETICLE_OFFSET+5.0f,
-                                       RETICLE_OFFSET+5.0f,
+                                       RETICLE_OFFSET+15.0f,
+                                       RETICLE_OFFSET+15.0f,
                                        3.0f,
                                        3.0f
                                        )
